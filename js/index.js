862a7253ffec057a3acf25181c52541913ca9fd1
IchemJ <ichem76610@hotmail.com>
2024-12-11 15:09:29 +0100

    exercice try

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("register").addEventListener("click", function () {
+    let titre = document.getElementById("titre").value
+    let text = document.getElementById("txt").value
+    if (titre != "" & text != "")
+        document.getElementById("post-it").innerHTML += `<div class="post-it"><button class="close"></button><h1 id="titres">${titre}</h1><p>${text}</p></div>`
+})
+document.getElementById("cancell").addEventListener("click", function(){
+    document.getElementById("titre").value=""
+    document.getElementById("txts").value=""
+})
