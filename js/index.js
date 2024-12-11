944e7812702d444cddbbfa8d90a6f18b1afc8395
IchemJ <ichem76610@hotmail.com>
2024-12-11 16:55:44 +0100

    exofini

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@ document.getElementById("register").addEventListener("click", function () {
     let titre = document.getElementById("titre").value
     let text = document.getElementById("txt").value
     if (titre != "" & text != "")
-        document.getElementById("post-it").innerHTML += `<div class="post-it"><button class="close"></button><h1 id="titres">${titre}</h1><p>${text}</p></div>`
+        document.getElementById("post-it").innerHTML += `<div class="post-it"><button class="close" onclick="supprimer(this)"></button><h1 id="titres">${titre}</h1><p>${text}</p></div>`
 })
-document.getElementById("cancell").addEventListener("click", function(){
-    document.getElementById("titre").value=""
-    document.getElementById("txts").value=""
+document.getElementById("cancell").addEventListener("click", function () {
+    document.getElementById("titre").value = ""
+    document.getElementById("txt").value = ""
 })
+function supprimer(el) {
+    el.parentElement.remove()
+}
