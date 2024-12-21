39bddde58d4e61a77c60e9a7da7f627123f54724
Ich-dev <ichem76610@hotmail.com>
2024-12-21 15:39:26 +0100

    voila ce que j'ai compris pour l'instant

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const removee = document.getElementById("suppr")
-
+const savedPostIt = {
+    "enTete": titre,
+    "texte": txt
+}
 
 document.getElementById("register").addEventListener("click", function () {
     let titre = document.getElementById("titre").value
@@ -7,6 +10,7 @@ document.getElementById("register").addEventListener("click", function () {
     if (titre != "" & text != "")
         document.getElementById("post-it").innerHTML += `<div class="post-it"><button class="close" onclick="supprimer(this)"></button><h1 id="titres">${titre}</h1><p>${text}</p></div>`
 })
+
 document.getElementById("cancell").addEventListener("click", function () {
     document.getElementById("titre").value = ""
     document.getElementById("txt").value = ""
@@ -18,3 +22,7 @@ function supprimer(el) {
 removee.addEventListener("click", () => {
     document.getElementById("post-it").innerHTML = ""
 })
+
+localStorage.post = JSON.stringify(savedPostIt)
+console.log(JSON.parse(localStorage.post));
+
